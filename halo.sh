5de0f81b7afb4e75b7a7f5394cb4ea9d64b09dcf
badgersnax <berretterry@gmail.com>
2015-11-18 14:35:59 -0800

    issue in script
    Ash said that he had an issue in his script that I am trying to correct

--- halo.sh
+++ halo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#if [ -z {$HALO_TAG+x}]; then /opt/cloudpassage/bin/configure --agent-key=$HALO_KEY; else /opt/cloudpassage/bin/configure --agent-key=$HALO_KEY --tag=$HALO_TAG --read-only=true;fi
-/opt/cloudpassage/bin/configure --agent-key=$HALO_KEY --tag=$HALO_TAG --read-only=true
+if [ -z {$HALO_TAG+x} ]; then /opt/cloudpassage/bin/configure --agent-key=$HALO_KEY; else /opt/cloudpassage/bin/configure --agent-key=$HALO_KEY --tag=$HALO_TAG --read-only=true;fi
+#/opt/cloudpassage/bin/configure --agent-key=$HALO_KEY --tag=$HALO_TAG --read-only=true
 /etc/init.d/cphalod start
